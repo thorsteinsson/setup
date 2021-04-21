49fd8a8c63651bba2e26fc571449570cd85e5f00
Aegir Thorsteinsson <aegir@thorsteinsson.is>
2021-04-21 10:24:07 +0000

    Add Brew to PATH after installing

--- apps.sh
+++ apps.sh
@@ -4,6 +4,11 @@
 if test ! $(which brew); then
   echo “Installing brew…”
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  # Brew path
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
   brew analytics off
 fi
 
